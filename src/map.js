4295b541edf3ed4845ca0e22f39d31bf361d0311
James <jaimersonaraujo@gmail.com>
2014-09-13 02:06:17 -0300

    I forgot how to developer

--- src/map.js
+++ src/map.js
@@ -9,17 +9,17 @@ var g = svg.append('g');
 
 d3.json('brasil.json', function(error, data){
   if(error) return console.log(error);
-  console.log(data);
 
   var subunits = topojson.feature(data, data.objects.uf),
-      projection = d3.geo.mercator().scale(600)
-        .translate([width * 1.3, height * 0.2]),
+      projection = d3.geo.mercator().scale(800)
+        .translate([width * 1.75, height * 0.2]),
       path = d3.geo.path().projection(projection);
 
 
   svg.selectAll('.uf')
     .data(subunits.features)
     .enter().append('path')
-    .attr('class', function(d) { return 'subunit ' + d.id})
+    .attr('class', function(d) { return 'subunit ' + d.id; })
+    .style('fill', function(d) { return 'rgba(' + 2 * d.id.charCodeAt(1) + ', 100, 100, 1.0)'; })
     .attr('d', path);
 });
